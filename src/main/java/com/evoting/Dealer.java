ad6f87b2aa6277cef032fd52ad8f17f7b7d2b363
sreetamparida <sreetamparida123@gmail.com>
2019-11-23 15:16:14 +0530

    update dealer

--- src/main/java/com/evoting/Dealer.java
+++ src/main/java/com/evoting/Dealer.java
@@ -11,7 +11,7 @@ import java.security.Security;
 import java.util.ArrayList;
 import java.util.*;
 public class Dealer {
-    public ArrayList<Voter> voter;
+    public HashMap<String, Voter> voter;
     public ArrayList<Candidate> candidate;
     public ArrayList<String> keys;
     public Transaction genesisTransaction;
@@ -22,7 +22,24 @@ public class Dealer {
     public Dealer(){
         wallet = new Wallet();
         candidate = new ArrayList<Candidate>();
-        voter = new ArrayList<Voter>();
+        voter = new HashMap<String, Voter>();
+    }
+
+    public void generateSecretShare(int noVoters){
+        Shamir shamir = new Shamir(1,noVoters);
+        candidateShare = new BigInteger[noVoters][this.candidate.size()];
+        for(int i = 0; i<this.candidate.size(); i++){
+            BigInteger secret = new BigInteger(candidate.get(i).wallet.publicKey.toString().getBytes());
+            BigInteger share[] = shamir.split(secret);
+            for(int j=0; j<noVoters; j++){
+                candidateShare[j][i] = share[j];
+            }
+        }
+    }
+
+    public void addVoter(String username){
+        voter.put(username, new Voter());
+        voter.get(username).setKeyShare(candidateShare[voter.size()-1]);
     }
 
 
